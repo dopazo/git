d60b45b396168eb62844f3e137f416b18c0a70fb
dopazo <diegoopazo@hotmail.com>
2017-11-13 14:36:42 -0300

    recorrección

--- Prueba2Conway/Prueba2Conway.py
+++ Prueba2Conway/Prueba2Conway.py
@@ -1,35 +1,9 @@
-#list.insert(0,llenar) #inicio
-def Conway(inicio):
-    list = inicio
-    print(inicio)
-    list =1,inicio
-    cuenta=list.count(inicio)
-    #list =cuenta,inicio
-    print(1,inicio) #[1 1]
-    for i in range(3):
-        cont=1
-        a=[1]
-        for n in range(len(list)):
-            num=list[n]
-            num2=list[n-1]
-            if (num==num2):
-                cont=cont+1
-            else:
-                a=a+[cont,num2]
-                cont=1
-            list=cont,a
-            a += [cont, num2]
+from itertools import groupby
 
-            print(list)
+def Conway(numero):
+    for _ in range(10):
+        print(numero)
+        numero = ''.join([str(len(list(i))) + k for k, i in groupby(numero)])
 
-
-#    i = 0
-    #while (i < len(list)):
-     #   num=list[i]
-#
- #       yield num
-  #      i += 1
-
-c=Conway(1)
-#print(list(c))
-#print(c)
+ini=input("Iniciar en que numero:")
+Conway(ini)
